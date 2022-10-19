97ba53c0b5108c35504786d7a8c1dcd521b101a6
Ilya Zhevlakov <ilya8bojik@gmail.com>
2022-10-19 12:48:55 +0400

    Create theme provider

--- app/src/main/java/ru/izhxx/dayplanner/theme/Theme.kt
+++ app/src/main/java/ru/izhxx/dayplanner/theme/Theme.kt
@@ -1,16 +1,27 @@
 package ru.izhxx.dayplanner.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+
+@Composable
+fun DayPlannerAppTheme(
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        LocalColorProvider provides lightPalette,
+        content = content
+    )
+}
+
+object AppTheme {
+    val dayPlannerColors: DayPlannerColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColorProvider.current
+}
+
+val LocalColorProvider = staticCompositionLocalOf<DayPlannerColors> {
+    error("No default colors provided")
+}
